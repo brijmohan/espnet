c41404619238de350e31a4394ec0b2e9a3180654
Brij Mohan Lal Srivastava <brij.srivastava@inria.fr>
2019-02-22 12:30:38 +0100

    extract erep

--- egs/librispeech/asr1/extract_erep.sh
+++ egs/librispeech/asr1/extract_erep.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 
-local/gen_erep.sh --data-in /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/train_960_combined_no_sil \
-	--data-out /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/erep_train_960_combined_no_sil \
+dataset=test_clean_trial
+
+local/gen_erep.sh --data-in /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/${dataset} \
+	--data-out /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/erep_${dataset} \
 	--modeldir exp/pretrained_e2e_train_960 \
-	--erep-dir /home/bsrivast/kaldi/egs/librispeech_spkv/v2/exp/erep_train_960_combined_no_sil
+	--erep-dir /home/bsrivast/kaldi/egs/librispeech_spkv/v2/exp/erep_${dataset}
--- egs/librispeech/asr1/local/gen_erep.sh
+++ egs/librispeech/asr1/local/gen_erep.sh
@@ -12,12 +12,12 @@ modeldir=
 erep_dir=
 
 recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
-nj=5
+nj=4
 train_set=data/train_960
 fbankdir=`pwd`/fbank
 # feature configuration
 do_delta=false
-stage=1
+stage=0
 
 . utils/parse_options.sh || exit 1;
 
@@ -44,10 +44,10 @@ if [ ${stage} -le 0 ]; then
 	fi
 	mkdir -p ${feat_dir}
 
-	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 --write_utt2num_frames true \
+	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 29 --write_utt2num_frames true \
 	    ${data_out} exp/make_fbank/${feats_name} ${fbankdir}
 
-	dump.sh --cmd "$train_cmd" --nj 32 --do_delta $do_delta \
+	dump.sh --cmd "$train_cmd" --nj 29 --do_delta $do_delta \
 		${data_out}/feats.scp ${train_set}/cmvn.ark exp/dump_feats/${feats_name} ${feat_dir}
 	
 fi
@@ -57,29 +57,28 @@ if [ ${stage} -le 1 ]; then
 	# Split feats.scp for parallel processing
 	split_dir=${feat_dir}/split${nj}feats
 
-#	if [ -d ${split_dir} ]; then
-#		rm -rf ${split_dir}
-#	fi
-#	mkdir -p ${split_dir}
-#	sfeats=$(for n in `seq $nj`; do echo ${split_dir}/feats.$n.scp; done)
-#	utils/split_scp.pl ${feat_dir}/feats.scp $sfeats
-#
-#	if [ -d ${erep_dir} ]; then
-#	  rm -rf ${erep_dir}
-#	fi
-#	mkdir -p ${erep_dir}
+	if [ -d ${split_dir} ]; then
+		rm -rf ${split_dir}
+	fi
+	mkdir -p ${split_dir}
+	sfeats=$(for n in `seq $nj`; do echo ${split_dir}/feats.$n.scp; done)
+	utils/split_scp.pl ${feat_dir}/feats.scp $sfeats
+
+	if [ -d ${erep_dir} ]; then
+	  rm -rf ${erep_dir}
+	fi
+	mkdir -p ${erep_dir}
 
 	#### use CPU for decoding
 	ngpu=1
 
 	# set batchsize 0 to disable batch decoding
-	#${decode_cmd} JOB=1:${nj} ${erep_dir}/log/erep.JOB.log \
-	${decode_cmd} ${erep_dir}/log/erep.4.log \
+	${decode_cmd} JOB=1:${nj} ${erep_dir}/log/erep.JOB.log \
 	    asr_encode.py \
 	    --ngpu ${ngpu} \
 	    --batchsize 0 \
-	    --feats-in ${split_dir}/feats.4.scp \
-	    --feats-out ${erep_dir}/erep_${feats_name}.4 \
+	    --feats-in ${split_dir}/feats.JOB.scp \
+	    --feats-out ${erep_dir}/erep_${feats_name}.JOB \
 	    --model ${modeldir}/${recog_model}  \
 	    &
 	wait
