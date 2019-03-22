b8ca81740aef42d635e41fc788dbe7eb2252ab5f
Brij Mohan Lal Srivastava <brij.srivastava@inria.fr>
2019-03-22 11:08:06 +0100

    code to extract erep

--- egs/librispeech/asr1/extract_erep.sh
+++ egs/librispeech/asr1/extract_erep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dataset=test_clean_trial
+dataset=train_plda_combined
 
 local/gen_erep.sh --data-in /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/${dataset} \
 	--data-out /home/bsrivast/kaldi/egs/librispeech_spkv/v2/data/erep_${dataset} \
 	--modeldir exp/pretrained_e2e_train_960 \
-	--erep-dir /home/bsrivast/kaldi/egs/librispeech_spkv/v2/exp/erep_${dataset}
+	--erep-dir /home/bsrivast/kaldi/egs/librispeech_spkv/v2/exp_ext/erep_${dataset}
--- egs/librispeech/asr1/local/gen_erep.sh
+++ egs/librispeech/asr1/local/gen_erep.sh
@@ -12,7 +12,7 @@ modeldir=
 erep_dir=
 
 recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
-nj=4
+nj=3
 train_set=data/train_960
 fbankdir=`pwd`/fbank
 # feature configuration
@@ -30,6 +30,9 @@ set -o pipefail
 
 feats_name=$(basename ${data_out})
 feat_dir=dump/${feats_name}/delta${do_delta}
+vadfeats=/media/data/vadfeats
+
+#mkdir -p ${vadfeats}
 
 if [ ${stage} -le 0 ]; then
 
@@ -44,10 +47,42 @@ if [ ${stage} -le 0 ]; then
 	fi
 	mkdir -p ${feat_dir}
 
-	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 29 --write_utt2num_frames true \
+	: '
+	# Compute fbank with energy
+	steps/make_fbank_pitch.sh --fbank-config conf/fbank_energy.conf --cmd "$train_cmd" --nj 29 --write_utt2num_frames true \
 	    ${data_out} exp/make_fbank/${feats_name} ${fbankdir}
 
-	dump.sh --cmd "$train_cmd" --nj 29 --do_delta $do_delta \
+	# Compute VAD
+	sid/compute_vad_decision.sh --nj 29 --cmd "$train_cmd" \
+		${data_out} exp/make_vad $vadfeats
+	utils/fix_data_dir.sh ${data_out}
+
+	# Store VAD somewhere else
+	cp ${data_out}/vad.scp local/
+	
+	# Make fbank without energy for ASR decoding
+	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 29 --write_utt2num_frames true \
+	    ${data_out} exp/make_fbank/${feats_name} ${fbankdir}
+	# Copy back the VAD
+	mv local/vad.scp ${data_out}/
+
+
+	# Apply VAD and select frames
+	apply-cmvn-sliding --norm-vars=false --center=true --cmn-window=300 scp:${data_out}/feats.scp ark:- | select-voiced-frames ark:- scp,s,cs:${data_out}/vad.scp ark,scp:${vadfeats}/${feats_name}.ark,${vadfeats}/${feats_name}.scp
+
+	# Use new feats for xvectors
+	cp ${vadfeats}/${feats_name}.scp ${data_out}/feats.scp
+	rm ${data_out}/vad.scp
+	'
+	# Make fbank without energy for ASR decoding
+	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 40 --write_utt2num_frames true \
+	    ${data_out} exp/make_fbank/${feats_name} ${fbankdir}
+	utils/fix_data_dir.sh ${data_out}
+	if [ -f ${data_out}/vad.scp ]; then
+	  rm ${data_out}/vad.scp
+	fi
+
+	dump.sh --cmd "$train_cmd" --nj 40 --do_delta $do_delta \
 		${data_out}/feats.scp ${train_set}/cmvn.ark exp/dump_feats/${feats_name} ${feat_dir}
 	
 fi
@@ -56,7 +91,6 @@ if [ ${stage} -le 1 ]; then
 
 	# Split feats.scp for parallel processing
 	split_dir=${feat_dir}/split${nj}feats
-
 	if [ -d ${split_dir} ]; then
 		rm -rf ${split_dir}
 	fi
@@ -73,6 +107,7 @@ if [ ${stage} -le 1 ]; then
 	ngpu=1
 
 	# set batchsize 0 to disable batch decoding
+	#${decode_cmd} ${erep_dir}/log/erep.3.log \
 	${decode_cmd} JOB=1:${nj} ${erep_dir}/log/erep.JOB.log \
 	    asr_encode.py \
 	    --ngpu ${ngpu} \
@@ -83,7 +118,8 @@ if [ ${stage} -le 1 ]; then
 	    &
 	wait
 
-	cat ${erep_dir}/*.scp > ${data_out}/erep_feats.scp
+	mv ${data_out}/feats.scp ${data_out}/fbank_feats.scp
+	cat ${erep_dir}/*.scp > ${data_out}/feats.scp
 fi
 
 echo "Finished"
